57982c55e6d09037729f4f6368b1efca225e2b16
Ayush Butala <butalaayush4.com>
2025-04-14 14:48:40 +0530

    Fix backend issue

--- Back-end/exel_to_json.py
+++ Back-end/exel_to_json.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import json
+import os
 
-df = pd.read_excel("updated_leaderboard.xlsx")
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-leaderboard_data = df[["Name", "Final Score"]].to_dict(orient="records")
+excel_path = os.path.join(base_dir, "Back-end", "updated_leaderboard.xlsx")
+df = pd.read_excel(excel_path)
 
-with open("leaderboard.json", "w") as json_file:
+leaderboard_data = df[["Name", "CGPA",  "LeetCode Score", "Final Score"]].to_dict(orient="records")
+
+frontend_dir = os.path.join(base_dir, "Front-end")
+json_path = os.path.join(frontend_dir, "leaderboard.json")
+
+with open(json_path, "w") as json_file:
     json.dump(leaderboard_data, json_file, indent=4)
 
-print("Leaderboard JSON file generated successfully!")
+print(f"Leaderboard JSON file saved at: {json_path}")
